f63387c7f923ea21750875c26a6fa321ab7f2b51
ajay <ajay.dev@neoito.com>
2022-11-03 15:48:10 +0530

    new pr test

---
+++ higher-logic/mongo-moment.js
@@ -0,0 +1,6 @@
+
+const moment = require("moment");
+console.log(moment("2022-11-03T09:11:51.212+00:00").format("yyy-MM-DD hh:mm:ss"));
+
+// const filterDate = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
+// console.log(filterDate)
